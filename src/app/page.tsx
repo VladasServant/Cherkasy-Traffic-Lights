a447edbbfec551521e970b22e03a6db48e692b35
VladasServant <95712610+VladasServant@users.noreply.github.com>
2025-04-09 23:37:34 +0300

    a little update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { MapProvider } from "@/providers/map-provider";
 import { MapComponent } from "@/components/map";
 
+export const metadata = {
+  title: "Cherkasy: Traffic Lights",
+}
+
 export default function Home() {
   return (
     <MapProvider>
